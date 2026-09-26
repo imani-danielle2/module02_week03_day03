e1551a3c0d99b2b787997720ff421ac18c9a527b
thriveinn <imanitvrc@gmail.com>
2026-09-26 10:35:28 -0500

    Update js

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+let myArray = ["Date", "is", "stored", "at","each", "index"];
+
+
+// Loop through the array and print each element
+// for (let i=0; myArray.length >= 5; i++)
+
+// Start of the loop
+   // {
+// Print the current element of the array
+  //      while (i < myArray.length) 
+            
+        //    {
+    //        console.log(myArray[i]);
+            i++;
+      //  }
+   
+    //}
+
+// console.log("Loop finished.");
+
+
+//write a function that multiplies two numbers
+function multiply(a, b) 
+
+{
+    let result = a * b;
+    console.log(result);
+}
+
+console.log(multiply(3, 4));
